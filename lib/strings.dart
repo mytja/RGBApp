c02f87ebcfb579b0b1ae75b0a73d9ab24992f443
exatonix <exatonix@users.noreply.github.com>
2021-02-16 00:34:18 +0300

    Update strings.dart
    Added Turkish Lang Strings
    and Updated.

--- lib/strings.dart
+++ lib/strings.dart
@@ -2,8 +2,11 @@ const String lang_en = "English";
 const String lang_en_us = "USA";
 const String lang_sl = "slovenščina";
 const String lang_sl_si = "Slovenija";
+const String lang_tr = "Turkish";
+const String lang_tr_tr = "Turkey";
 
 String lang_v = "en_us";
+String lang_v = "tr_tr"; 
 
 String wnl;
 String yes;
@@ -36,5 +39,15 @@ void updateGlobals() {
     r = "R";
     g = "G";
     b = "B";
-  }
+  } else if (lang_v == "tr_tr") {
+    wnl = "Yeni hat ile?";
+    yes = "Evet";
+    no = "Hayır";
+    lang = "Dil";
+    res = "Çözünürlük (bit bazında)";
+    disconnect = "BAĞLANTIYI KES";
+    r = "R";
+    g = "G";
+    b = "B";
+ } 
 }
